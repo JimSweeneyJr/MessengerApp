c5c551d8eda5e2b373996da7612fdf28a288f7ae
JimSweeneyJr <jimsweeney566@gmail.com>
2023-08-15 21:47:17 -0400

    First Commit

---
+++ MessengerApp/MessengerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerAppApp.swift
+//  MessengerApp
+//
+//  Created by James Sweeney on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct MessengerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
